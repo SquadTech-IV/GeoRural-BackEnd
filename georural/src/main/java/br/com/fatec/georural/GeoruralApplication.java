00c07abcdc751299a0be59f95800469e8544aa00
Natanael <natanaelsm0102@gmail.com>
2026-09-13 11:57:10 -0300

    chore: initial project structure with docker, spring boot and maven dependencies

---
+++ georural/src/main/java/br/com/fatec/georural/GeoruralApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.georural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoruralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoruralApplication.class, args);
+	}
+
+}
---
+++ georural/src/test/java/br/com/fatec/georural/GeoruralApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.georural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoruralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
